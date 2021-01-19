4e52b70643433d695e7cfb6be8aa46707eef6706
Aidan Murray <aidanhansm@gmail.com>
2021-01-18 23:21:17 -0700

    Add map and weather services

---
+++ app/services/map_service.rb
@@ -0,0 +1,18 @@
+class MapService
+  def self.search(location)
+    response = conn.get('geocoding/v1/address') do |req|
+      req.params[:location] = location
+    end
+
+    JSON.parse(response.body, symbolize_keys: true)
+  end
+
+  def self.conn
+    Faraday.new('https://www.mapquestapi.com/') do |req|
+      req.params[:key] = ENV['MAPQUEST_API_KEY']
+      req.params[:inFormat] = 'kvp'
+      req.params[:inFormat] = 'json'
+      req.params[:thumbMaps] = false
+    end
+  end
+end
