372b78d86b69cdebac3f04c82654ec464535dda8
Aidan Murray <aidanhansm@gmail.com>
2021-01-19 09:27:24 -0700

    Add route for munchies

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast/', to: 'weather#show', as: :forecast
+      get '/munchie', to: 'munchies#index', as: :munchie
     end
   end
 end
